ef3212850aecceabdd17cec0ea96ad0d58d04089
Damian Trafiałek <damian.trafialek@gmail.com>
2023-04-18 18:44:08 +0200

    add: palette route id

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from "react";
-import Palette from "./Palette";
-import seedColors from "./seedColors";
-import { generatePalette } from "./colorHelpers";
 import { Route } from 'react-router-dom';
 import { Routes } from "react-router-dom";
+import Sth from "./Sth";
+import PaletteWithProps from "./PalleteWithProps";
+
 class App extends Component {
+
   render() {
-    console.log(generatePalette(seedColors[4]));
     return (
+
       <Routes>
         <Route exact path="/" element={<h1>Palette list goes here</h1>} />
-        <Route path="/palette/:id" element={<h1>Invidaula Palette</h1>} />
-
-        {/* <Palette palette={generatePalette(seedColors[4])} /> */}
+        <Route path="/palette/:id" element={<PaletteWithProps />} />
       </Routes>
     );
   }
 }
+
+
+
 
 export default App;
---
+++ src/PalleteWithProps.js
@@ -0,0 +1,19 @@
+import Palette from "./Palette";
+import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
+import { useParams } from "react-router-dom";
+
+function PaletteWithProps() {
+    const { id } = useParams();
+
+    function findPalette(id) {
+        return seedColors.find(function (palette) {
+            return palette.id === id;
+        })
+    }
+    console.log(id, findPalette(id))
+
+    return <Palette palette={generatePalette(findPalette(id))} />
+}
+
+export default PaletteWithProps
