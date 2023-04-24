d729f37bd096cad99166a16a46e35c489f11d7a8
Damian Trafiałek <damian.trafialek@gmail.com>
2023-04-24 21:57:23 +0200

    add: bacis minipalette styles

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -11,12 +11,9 @@ export default class PaletteList extends Component {
         const { palettes } = this.props
         return (
             <div className='PaletteList'>
-                <MiniPalette />
                 <h1>React Colors</h1>
                 {palettes.map(palette => (
-                    <p key={palette.id}>
-                        <Link key={palette.id} to={`/palette/${palette.id}`} >{palette.paletteName}</Link>
-                    </p>
+                    <MiniPalette key={palette.id} {...palette} />
                 ))}
             </div>
         )
