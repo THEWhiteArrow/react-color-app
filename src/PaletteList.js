89b086a6fda2f0b295d658a5528a967067c9a841
Damian Trafiałek <damian.trafialek@gmail.com>
2023-04-28 13:39:12 +0200

    add: start styling palette list

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,22 +1,57 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import MiniPalette from './MiniPalette'
+import { createUseStyles } from 'react-jss'
 
-export default class PaletteList extends Component {
-    constructor(props) {
-        super(props)
+const styles = createUseStyles({
+    root: {
+        backgroundColor: 'blue',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    container: {
+        width: '50%',
+        display: 'flex',
+        alignItems: 'flex-start',
+        flexDirection: 'column',
+        flexWrap: 'wrap',
+    },
+    nav: {
+        display: 'flex',
+        width: '100%',
+        justifyContent: 'space-between',
+        color: 'white'
+    },
+    palettes: {
+        boxSizing: 'border-box',
+        width: '100%',
+        display: 'grid',
+        gridTemplateColumns: 'repeat(3,30%)',
+        gridGap: '5%'
     }
+})
 
-    render() {
-        const { palettes } = this.props
-        return (
-            <div className='PaletteList'>
-                <h1>React Colors</h1>
-                {palettes.map(palette => (
-                    <MiniPalette key={palette.id} {...palette} />
-                ))}
+export default function PaletteList(props) {
+
+
+    const classes = styles()
+    const { palettes } = props
+    return (
+        <div className={classes.root}>
+            <div className={classes.container}>
+                <nav className={classes.nav}>
+                    <h1>React Colors</h1>
+                </nav>
+                <div className={classes.palettes}>
+                    {palettes.map(palette => (
+                        <MiniPalette key={palette.id} {...palette} />
+                    ))}
+                </div>
             </div>
-        )
-    }
+        </div>
+    )
+
 }
 
