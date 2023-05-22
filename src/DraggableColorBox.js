43f38b212dac1b383a51260cb6c2be81e6f8d38f
Damian Trafiałek <damian.trafialek@gmail.com>
2023-05-22 13:12:40 +0200

    add: style draggable color box

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import './DraggableColorBox.css'
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function DraggableColorBox(props) {
-    return <div className='DraggableColorBox' style={{ backgroundColor: props.color, wordBreak: 'break-word' }}>{props.name}</div>
+    return (
+        <div className='DraggableColorBox' style={{ backgroundColor: props.color, wordBreak: 'break-word' }}>
+
+            <div className='box-content'>
+                <span>
+                    {props.name}
+                </span>
+                <DeleteIcon className='delete-icon' />
+            </div>
+        </div>
+    )
 }
